61e568db3d37aa39f4bea7b525b366cc06ea978d
mauroalvare <149129640+mauroalvare@users.noreply.github.com>
2026-08-05 19:05:56 -0300

    Initial commit

---
+++ src/Program/Program.cs
@@ -0,0 +1,10 @@
+﻿public static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(Addition.Add(1, 2));
+        Console.WriteLine(Subtraction.Subtract(3, 4));
+        Console.WriteLine(Multiplication.Multiply(5, 6));
+        Console.WriteLine(Division.Divide(7, 8));
+    }
+}
