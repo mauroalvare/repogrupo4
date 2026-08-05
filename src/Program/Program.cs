47eab19f2c0aba100b797ccfea24d77b4d8ddff8
Victoria <mariavictoria.melo@correo.ucu.edu.uy>
2026-08-05 19:29:09 -0300

    Implementación de la operación división.

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -17,3 +17,12 @@ public class Subtraction
        return a - b;
    }
 }
+
+// Esta clase implementa la operación división.
+public class Division
+{
+   public static double Divide(int a, int b)
+   {
+       return (double)a / b;
+   }
+}
