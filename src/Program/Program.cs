52a288d77a00b27fd56c59f08c2b2ae220578693
IgnacioNoble-R <134978935+IgnacioNoble-R@users.noreply.github.com>
2026-08-10 20:05:31 -0300

    Implementacion de la operaion suma

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -46,3 +46,12 @@ public class Division
         return (double)a / b;
     }
 }
+
+// Esta clase implementa la operación suma
+public class Addition
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
