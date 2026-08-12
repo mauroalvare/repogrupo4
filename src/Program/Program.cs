2f95ccff02f9f7c96d30b6adfc9658330fae0e98
IgnacioNoble-R <134978935+IgnacioNoble-R@users.noreply.github.com>
2026-08-12 19:06:14 -0300

    Corrige clase Addition duplicada

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -46,23 +46,3 @@ public class Division
         return (double)a / b;
     }
 }
-
-// Esta clase implementa la operación suma
-public class Addition
-{
-    public static int Add(int a, int b)
-    {
-        return a + b;
-    }
-﻿public static class Program
-{
-    public static void Main()
-    {
-        Console.WriteLine(Addition.Add(1, 2));
-        Console.WriteLine(Subtraction.Subtract(3, 4));
-        Console.WriteLine(Multiplication.Multiply(5, 6));
-        Console.WriteLine(Division.Divide(7, 8));
-    }
-
-}
-// 
