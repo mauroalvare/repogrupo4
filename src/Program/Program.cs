5ccd43b49eecda78143be82c297c1d9f98011c0e
IgnacioNoble-R <134978935+IgnacioNoble-R@users.noreply.github.com>
2026-08-12 18:46:01 -0300

    Cambio incorrecto

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,4 @@
-﻿﻿namespace Ucu.Poo.GitChallenge;
+﻿namespace Ucu.Poo.GitChallenge;
 
 public static class Program
 {
@@ -16,7 +16,7 @@ public class Addition
 {
     public static int Add(int a, int b)
     {
-        return a + b;
+        return 0; // Cambio incorrecto
     }
 }
 
@@ -47,29 +47,3 @@ public class Division
     }
 }
 
-// Esta clase implementa la operación suma
-public class Addition
-{
-    public static int Add(int a, int b)
-    {
-        return a + b;
-    }
-﻿public static class Program
-{
-    public static void Main()
-    {
-        Console.WriteLine(Addition.Add(1, 2));
-        Console.WriteLine(Subtraction.Subtract(3, 4));
-        Console.WriteLine(Multiplication.Multiply(5, 6));
-        Console.WriteLine(Division.Divide(7, 8));
-    }
-
-}
-// Esta clase implementa la operación suma
-public class Addition
-{
-   public static int Add(int a, int b)
-   {
-       return a + b;
-   }
-}
